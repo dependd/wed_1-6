e5ee04f8289910141a095acb29f3cff0aea84f47
matsuda <darby8928@gmail.com>
2019-09-25 12:02:58 +0900

    画面端からの端への移動を統合

--- Assets/Tomioka/Warp.cs
+++ Assets/Tomioka/Warp.cs
@@ -12,19 +12,20 @@ public class Warp : MonoBehaviour
     private void Update()
     {
         _playerPos = _player.transform.position;
+        WarpReverse();
     }
 
     public void WarpReverse()
     {
         //左側にいる
-        if (_playerPos.x < 0)
+        if (_playerPos.x < -6.5)
         {
             Debug.Log("左側にいる");
             _player.transform.position = new Vector3(6.5f, _playerPos.y, _playerPos.z);
 
         }
         //右側にいる
-        else if (_playerPos.x > 0)
+        else if (_playerPos.x > 6.5)
         {
             Debug.Log("右側にいる");
             _player.transform.position = new Vector3(-6.5f, _playerPos.y, _playerPos.z);
