6b683de7536a57369c494dd0c6128fb2d9756782
Tomioka <>
2019-09-25 11:52:58 +0900

    プレイヤーが画面端に行くと反対側に行くようにする(現状はボタンで呼んでる状態) 画面端に当たり判定をつけてこの関数を呼ぶようにする

---
+++ Assets/Tomioka/Warp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    private Vector3 _playerPos;
+
+    private void Update()
+    {
+        _playerPos = _player.transform.position;
+    }
+
+    public void WarpReverse()
+    {
+        //左側にいる
+        if (_playerPos.x < 0)
+        {
+            Debug.Log("左側にいる");
+            _player.transform.position = new Vector3(6.5f, _playerPos.y, _playerPos.z);
+
+        }
+        //右側にいる
+        else if (_playerPos.x > 0)
+        {
+            Debug.Log("右側にいる");
+            _player.transform.position = new Vector3(-6.5f, _playerPos.y, _playerPos.z);
+        }
+
+    }
+
+
+}
