57591b677cc39cd34c906fdb54ea4a338c95f531
NNH2512 <nguyendinhminhduc19122014@gmail.com>
2019-09-25 16:04:26 +0900

    animationがうまくできない。

---
+++ Assets/Scripts/LeftController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftController : MonoBehaviour
+{
+    private int left = 3;
+    public void LeftMinus()
+    {
+        left--;
+        Debug.Log(left);
+        if (left == -1)
+        {
+            //GameOver
+            Debug.Log("GameOver");
+        }
+    }
+}
