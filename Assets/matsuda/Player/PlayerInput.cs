0706ecb475523e9f95957996901e4aab78a79528
matsuda <darby8928@gmail.com>
2019-09-25 11:45:31 +0900

    攻撃のAnimationと判定を取る元の作成。cubeのハンマー作成

--- Assets/matsuda/Player/PlayerInput.cs
+++ Assets/matsuda/Player/PlayerInput.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     Rigidbody rg;
+    Player playerComp;
 
     float MoveSpeed = 10 / 4;
     float speed;
 
-    public bool isAction,isAir;
+    public bool isAir;
 
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody>();
+        playerComp = GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -22,26 +24,28 @@ public class PlayerInput : MonoBehaviour
     {
         speed = rg.velocity.magnitude;
         //ジャンプ
-        if (Input.GetKeyDown(KeyCode.X) && !isAction && !isAir)
+        if (Input.GetKeyDown(KeyCode.X) && !playerComp.isAction && !isAir)
         {
             rg.AddForce(new Vector3(0, 500, 0));
             MoveSpeed = MoveSpeed / 4;
         }
         //ハンマー
-        else if (Input.GetKeyDown(KeyCode.Z) && !isAction && !isAir)
+        else if (Input.GetKeyDown(KeyCode.Z) && !playerComp.isAction && !isAir)
         {
-
+            playerComp.PAttack();
             //ハンマー攻撃の関数
         }
 
         //スピードに制限をかける
-        if (speed >= 2 || isAction) return;
+        if (speed >= 2 || playerComp.isAction) return;
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rg.AddForce(new Vector3(MoveSpeed,0,0));
-        }else if (Input.GetKey(KeyCode.LeftArrow))
+            transform.localRotation = new Quaternion(0,0,0,0);
+        } else if (Input.GetKey(KeyCode.LeftArrow))
         {
             rg.AddForce(new Vector3(-MoveSpeed, 0, 0));
+            transform.localRotation = new Quaternion(0, 180, 0, 0);
         }
     }
 
@@ -55,12 +59,4 @@ public class PlayerInput : MonoBehaviour
         isAir = true;
     }
     
-    IEnumerator PAttack()
-    {
-        yield return new WaitForSeconds(0.25f);
-        GameObject obj = new GameObject();  //攻撃用コライダーがついてるオブジェクトにする
-        obj.SetActive(true);
-        yield return new WaitForSeconds(0.25f);
-        obj.SetActive(false);
-    }
 }
