daf66533d8810cd2b7d4a00fcc4cb07ce42e6826
asahi <asahii.jr@gmail.com>
2019-09-25 17:33:11 +0900

    適当にバード実装

---
+++ Assets/menber/asahi/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : Enemy
+{
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+    
+    protected override void Move(){
+        this.transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    protected override void DownMove(){
+
+        this.transform.position += new Vector3(0.0f, -2 * Time.deltaTime, 0.0f);
+
+    }
+
+
+}
