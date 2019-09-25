2c89819876df296257718b81acf2479aaf965942
kazuya <yuuki0523yata@gmail.com>
2019-09-25 14:10:49 +0900

    動かすのは完成 プレイヤーの子付けをこれから

---
+++ Assets/iwasaki_k/Scripts/MFJenerater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MFJenerater : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _floar;
+    //intの値に呼び出す動く床の番号(0番が左から動く床/1番が右から動く床)
+    //[0番のvec3が(-8,0,0)]/[1番のvec3が(8,0,0)]
+    private void Start()
+    {
+        Instantiate(_floar[0], new Vector3(-8,0,0), Quaternion.identity);
+    }
+    public void MFJenerate(int num,Vector3 _floarPos)
+    {
+        Instantiate(_floar[num], _floarPos, Quaternion.identity);
+    }
+}
--- Assets/iwasaki_k/Scripts/MoveFloar.cs
+++ Assets/iwasaki_k/Scripts/MoveFloar.cs
@@ -9,31 +9,33 @@ public class MoveFloar : MonoBehaviour
     //子にするためのプレイヤーを格納する
     private GameObject _childPlayer;
 
-    private Vector3 force;
+    private Vector3 force, _firstPos;
 
+    private bool rdAddCheck;
     //床の動く速度
     [SerializeField] private float _moveSpeed = 5f;
 
-    private Rigidbody rd;
-    private void Start()
-    {
-        rd = GetComponent<Rigidbody>();
-    }
+    [HideInInspector] public Rigidbody rd;
     private void Update()
     {
-
+        if (!rdAddCheck)
+        {
+            rd = GetComponent<Rigidbody>();
+            _firstPos = this.gameObject.transform.position;
+            rdAddCheck = true;
+        }
         //床の移動制御
         switch (this.gameObject.name)
         {
-            case "MF_RtoL":
+            case "MF_RtoL(Clone)":
+                //Debug.Log("呼ばれてる");
                 transform.position -= transform.right * _moveSpeed * Time.deltaTime;
+                if (this.transform.position.x < -8) { transform.position = _firstPos; }
                 break;
-            case "MF_LtoR":
+            case "MF_LtoR(Clone)":
                 transform.position += transform.right * _moveSpeed * Time.deltaTime;
+                if (this.transform.position.x > 8) { transform.position = _firstPos; }
                 break;
         }
-    }
-    private void OnTriggerEnter(Collider tricol)
-    {
     }
 }
