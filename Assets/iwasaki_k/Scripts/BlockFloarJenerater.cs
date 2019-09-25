6549cff380773004e98451632947b13c1e3da6df
yata <yuuki0523yata@gmail.com>
2019-09-25 18:48:07 +0900

    二段目のまばらに生成がおわってない

--- Assets/iwasaki_k/Scripts/BlockFloarJenerater.cs
+++ Assets/iwasaki_k/Scripts/BlockFloarJenerater.cs
@@ -8,16 +8,28 @@ public class BlockFloarJenerater : MonoBehaviour
     [SerializeField] private GameObject _fbParent;
     [SerializeField] private float instantXPos = 4.75f;
 
+    private List<int> randomList = new List<int>();
+    private List<GameObject> blocs = new List<GameObject>();
     private int blockNum;
-    
+
     void Start()
     {
-        for(int x = 0; x < 20; x++)
+        //一列生成
+        for (int x = 0; x < 20; x++)
         {
-            Instantiate(_floarBlock, new Vector3(instantXPos, 3, 0), Quaternion.identity).transform.parent = _fbParent.transform; ;
+            Instantiate(_floarBlock, new Vector3(instantXPos, 4, 0), Quaternion.identity).transform.parent = _fbParent.transform;
+            blocs.Add(_floarBlock);
             instantXPos -= 0.5f;
         }
-
+        //まばらに生成
+        int posnum = Random.Range(5, 14);
+        for (int y = 0; y <= posnum; y++)
+        {
+            int instantNum = Random.Range(0, _fbParent.transform.childCount);
+            if (randomList.Contains(instantNum)) { continue; }
+            Instantiate(blocs[instantNum], new Vector3(, 3.5f, 0), Quaternion.identity);
+            randomList.Add(instantNum);
+        }
     }
     public void BFJenerate(float instanceYPos)
     {
