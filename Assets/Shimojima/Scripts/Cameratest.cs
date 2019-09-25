14480c14fe54c39b7fbe806572bad9d2f2186d5a
shimojima <sigkalra8888@gmail.com>
2019-09-25 11:04:24 +0900

    [New]カメラのスクロール

---
+++ Assets/Shimojima/Scripts/Cameratest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameratest : MonoBehaviour
+{
+    [SerializeField]
+    private bool moveON = false;
+
+    [Range(0.01f,1)]
+    public float speed;
+
+    [SerializeField]
+    private int stageNum;
+    public float[] stageHight;
+    private float high = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (!moveON)
+            {
+                if (stageHight.Length != stageNum)
+                {
+                    stageNum++;
+                    moveON = true;
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (moveON)
+        {
+            if (transform.position.y < stageHight[stageNum - 1] - 0.5f)
+            {
+                high += speed;
+                transform.position = new Vector3(0, high, -10);
+            }
+            else if (transform.position.y >= stageHight[stageNum - 1] - 0.5f)
+            {
+                moveON = false;
+            }
+        }
+    }
+}
