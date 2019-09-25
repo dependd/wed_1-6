5b5cb56937f98c0eb3b4173e278761e6e7feb434
asahi <asahii.jr@gmail.com>
2019-09-25 11:22:33 +0900

    エネミーの基礎クラスを作成

---
+++ Assets/menber/asahi/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//エネミーの基礎クラス
+public abstract class Enemy : MonoBehaviour
+{
+
+    //飛行タイプかどうか
+    protected bool flyType = false;
+
+    //移動速度
+    protected float speed = 1.0f;
+
+    //やられているか
+    protected bool down = false;
+
+    public bool downFlag{
+        get { return down; }
+    }
+    
+
+    // Update is called once per frame
+   　public virtual void Update()
+    {
+        if (!downFlag) {
+            Move();
+        }
+        else if(downFlag){
+            DownMove();
+        }
+    }
+
+    protected abstract void Move();
+
+    protected abstract void DownMove();
+
+    public void Down() {
+        down = true;
+    }
+
+}
